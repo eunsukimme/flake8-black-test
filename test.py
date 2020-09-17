af289eabf8dea2d797c15b711250af76748c08fc
Eunsu Kim <eunsu.dev@gmail.com>
2020-09-17 17:14:09 +0900

    passed lint and black formatting

--- test.py
+++ test.py
@@ -1,36 +1,5 @@
-def text():
+def test():
     print("hello world")
 
 
-def hello(name):
-    print(f"Hi, I am {name}.")
-
-
-def sample_function(
-    template,
-    file_path,
-    header,
-    a,
-    b,
-    c,
-    d,
-    e,
-    f,
-    g,
-    h,
-    i,
-    j,
-    debug=None,
-    db_sync=False,
-    *args,
-    **kwargs,
-):
-    pass
-
-
 a = [1, 2, 3, 4, 5]
-
-text()
-hello("eunsu")
-
-hello("david")
