5713b3436be3246680d7289fda8844613507e6c5
Eunsu Kim <eunsu.dev@gmail.com>
2020-09-17 17:03:44 +0900

    Initial Commit

---
+++ test.py
@@ -0,0 +1,36 @@
+def text():
+    print("hello world")
+
+
+def hello(name):
+    print(f"Hi, I am {name}.")
+
+
+def sample_function(
+    template,
+    file_path,
+    header,
+    a,
+    b,
+    c,
+    d,
+    e,
+    f,
+    g,
+    h,
+    i,
+    j,
+    debug=None,
+    db_sync=False,
+    *args,
+    **kwargs,
+):
+    pass
+
+
+a = [1, 2, 3, 4, 5]
+
+text()
+hello("eunsu")
+
+hello("david")
